a0618be5f1c0889bf203253a999eda7710b6a1cc
katwhite <quataridevinni@gmail.com>
2025-10-01 21:58:00 +0300

    добавлены команды cypress, убрано дублирование

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -7,7 +7,7 @@ describe('тесты для конструктора', function() {
     cy.setCookie('accessToken', 'test-access');
     window.localStorage.setItem('refreshToken', 'test-refresh');
 
-    cy.visit('http://localhost:4000');
+    cy.visit('/');
     cy.wait('@getUser');
     cy.wait('@getIngredients');
 
@@ -18,48 +18,60 @@ describe('тесты для конструктора', function() {
     cy.clearCookies();
   });
 
+  Cypress.Commands.add('getIngredientItem', () => {
+    cy.get('[data-cy=ingredient-item]')
+});
+
+Cypress.Commands.add('getModal', () => {
+    cy.get('[data-cy=modal]')
+});
+
+Cypress.Commands.add('getPrice', () => {
+    cy.get('[data-cy="constructor-price"]')
+});
+
   it('добавление булки и ингридиента из списка в конструктор', () => {
     const bun = "Краторная булка N-200i";
     const bunPrice = 1255;
     const ingredient = "Биокотлета из марсианской Магнолии";
     const ingredientPrice = 424;
     const totalPrice = bunPrice * 2 + ingredientPrice;
-    cy.get('[data-cy=ingredient-item]').contains(bun).as('bunIngredient');
+    cy.getIngredientItem().contains(bun).as('bunIngredient');
     cy.get('@bunIngredient').parent().find('button').click();
-    cy.get('[data-cy="constructor-price"]').should('contain', `${bunPrice * 2}`);
+    cy.getPrice().should('contain', `${bunPrice * 2}`);
     cy.get('[data-cy=constructor-bun]').contains(`${bun} (верх)`).should('exist');
     cy.get('[data-cy=constructor-bun]').contains(`${bun} (низ)`).should('exist');
-    cy.get('[data-cy=ingredient-item]').contains(bun).parent().find('.counter').should('contain.text', '2');
+    cy.getIngredientItem().contains(bun).parent().find('.counter').should('contain.text', '2');
 
-    cy.get('[data-cy=ingredient-item]').contains(ingredient).as('ingredient');
+    cy.getIngredientItem().contains(ingredient).as('ingredient');
     cy.get('@ingredient').parent().find('button').click();
-    cy.get('[data-cy="constructor-price"]').should('contain', totalPrice);
+    cy.getPrice().should('contain', totalPrice);
     cy.get('[data-cy=constructor-item]').contains(ingredient).should('exist');
-    cy.get('[data-cy=ingredient-item]').contains(ingredient).parent().find('.counter').should('contain.text', '1');
+    cy.getIngredientItem().contains(ingredient).parent().find('.counter').should('contain.text', '1');
   });
 
 describe('модальные окна ингредиентов', () => {
 
   it('открывает модалку ингредиента при клике', () => {
     const bun = "Краторная булка N-200i";
-    cy.get('[data-cy=ingredient-item]').contains(bun).click();
-    cy.get('[data-cy=modal]').should('exist');
-    cy.get('[data-cy=modal]').contains(bun);
+    cy.getIngredientItem().contains(bun).click();
+    cy.getModal().should('exist');
+    cy.getModal().contains(bun);
     cy.contains(bun).should('exist');
   });
 
   it('закрывает модалку по клику на крестик', () => {
-    cy.get('[data-cy=ingredient-item]').first().click();
-    cy.get('[data-cy=modal]').should('exist');
+    cy.getIngredientItem().first().click();
+    cy.getModal().should('exist');
     cy.get('[data-cy=modal-close]').click();
-    cy.get('[data-cy=modal]').should('not.exist');
+    cy.getModal().should('not.exist');
   });
 
   it('закрывает модалку по клику на оверлей', () => {
-    cy.get('[data-cy=ingredient-item]').first().click();
-    cy.get('[data-cy=modal]').should('exist');
+    cy.getIngredientItem().first().click();
+    cy.getModal().should('exist');
     cy.get('[data-cy=modal-overlay]').click({ force: true });
-    cy.get('[data-cy=modal]').should('not.exist');
+    cy.getModal().should('not.exist');
   });
 });
 
@@ -67,21 +79,21 @@ describe('модальные окна ингредиентов', () => {
     const bun = "Краторная булка N-200i";
     const orderNumber = 123;
 
-    cy.get('[data-cy=ingredient-item]').contains(bun).as('bunIngredient');
+    cy.getIngredientItem().contains(bun).as('bunIngredient');
     cy.get('@bunIngredient').parent().find('button').click();
 
     cy.get('[data-cy=order-button]').click();
 
     cy.wait('@createOrder');
 
-    cy.get('[data-cy=modal]').should('exist');
-    cy.get('[data-cy=modal]').contains(orderNumber);
+    cy.getModal().should('exist');
+    cy.getModal().contains(orderNumber);
 
     cy.get('[data-cy=modal-close]').click();
 
     cy.contains('Выберите булки').should('exist');
     cy.contains('Выберите начинку').should('exist');
-    cy.get('[data-cy="constructor-price"]').should('contain', '0');
+    cy.getPrice().should('contain', '0');
   });
 
 });
